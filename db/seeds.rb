28faf6047dd77c8b11e6e9ad78eb0e46f361b5de
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-09-01 11:06:34 +0200

    in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,15 @@ category_three = Category.new(name: "Swim")
 category_three.save!
 category_four = Category.new(name: "Sightseeing")
 category_four.save!
-<<<<<<< HEAD
-=======
 
 Trip.create(user: User.last, category: category_one)
 Trip.create(user: User.last, category: category_two)
 Trip.create(user: User.last, category: category_three)
 Trip.create(user: User.last, category: category_four)
->>>>>>> master
+
+booking_one = Booking.create(user: Trip.last, price: "50")
+booking_one.save!
+booking_two = Booking.create(user: Trip.last, price: "90")
+booking_two.save!
+booking_three = Booking.create(user: Trip.last, price: "30")
+booking_three.save!
