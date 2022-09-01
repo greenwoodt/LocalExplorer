be20c22121105fb146778c289b82e9cd8aef6207
SionaMarlin <snmarlin@gmail.com>
2022-09-01 10:18:40 +0200

    geolocation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,5 +3,6 @@ class Trip < ApplicationRecord
   belongs_to :category
   has_one_attached :photo
   validates :start_date, :end_date, presence: true
-
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
