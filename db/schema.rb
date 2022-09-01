cc2a1e2b1eab5889b2ae0a198878606e5dd8bec5
SionaMarlin <snmarlin@gmail.com>
2022-09-01 10:42:18 +0200

    latest merge geo stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema[7.0].define(version: 2022_08_31_094659) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_162933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
