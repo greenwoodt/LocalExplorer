0628b95a9f2592b1c712d6f806d3a37767ca3ea9
Tom Greenwood <tom.t.greenwood@gmail.com>
2022-09-04 17:30:16 +0200

    two avatars added to the map css

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,7 +10,7 @@ class TripsController < ApplicationController
         lat: trip.latitude,
         lng: trip.longitude,
         info_window: render_to_string(partial: "info_window", locals: { trip: trip }),
-        image_url: helpers.asset_url("logo.png")
+        image_url: helpers.asset_url("person-hiking-solid.svg")
       }
     end
   end
@@ -22,7 +22,7 @@ class TripsController < ApplicationController
       lat: @trip.latitude,
       lng: @trip.longitude,
       info_window: render_to_string(partial: "info_window", locals: { trip: @trip }),
-      image_url: helpers.asset_url("logo.png")
+      image_url: helpers.asset_url("person-hiking-solid.svg")
     }]
     @chatroom = Booking.where(trip: @trip).first&.chatroom
     @booking = Booking.new  # to be able to avoid the "new" view with a form
